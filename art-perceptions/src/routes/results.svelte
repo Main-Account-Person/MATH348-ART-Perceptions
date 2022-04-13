5b0c8d7dfdc965aa111b01c7dd207f3ffcd84150
Chris Lee <lee.chris.0888@gmail.com>
2022-04-13 16:56:24 -0400

    Results fix

--- art-perceptions/src/routes/results.svelte
+++ art-perceptions/src/routes/results.svelte
@@ -1,23 +1,27 @@
-<script>
-	import Chart from './LineChart.svelte';
+<script lang="ts">
+	// import Chart from './LineChart.svelte';
 </script>
 
-<div class="grid">
+
+<body>
+	<h1>Results</h1>
+</body>
+<!-- <div class="grid">
 	<div style="width: 33.3%; height: 33.3%"><Chart/></div>
 	<div style="width: 66.6%; height: 33.3%"><Chart/></div>
 	<div style="width: 33.3%; height: 66.6%"><Chart/></div>
 	<div style="width: 66.6%; height: 66.6%"><Chart/></div>
-</div>
+</div> -->
 
 <style>
-	:global(body) {
+	/* :global(body) {
 		padding: 0;
-	}
+	} */
 
-	.grid {
+	/* .grid {
 		width: 100vw;
 		height: 100vh;
 		grid-template-columns: repeat(3, 1fr);
 		grid-template-rows: repeat(3, 1fr);
-	}
+	} */
 </style>
