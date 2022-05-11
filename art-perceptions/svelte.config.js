a5767bd5119613845c998a645bc6cce100c99fb1
Main-Account-Person <meheretkourgessa4@gmail.com>
2022-05-11 09:18:49 -0400

    Json reading

--- art-perceptions/svelte.config.js
+++ art-perceptions/svelte.config.js
@@ -1,6 +1,25 @@
 import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
+import { config } from 'dotenv';
+import replace from '@rollup/plugin-replace';
+import json from '@rollup/plugin-json'
 
+export default {
+	plugins: [
+	replace({
+					__api: JSON.stringify({
+							env: {
+									isProd: production,
+									...config().parsed // attached the .env config
+							}
+					}),
+					delimiters: ['', '']
+			}),
+			json(),
+	// ...
+	],
+	// ...
+}
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
